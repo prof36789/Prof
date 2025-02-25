b64253761e09b9c98f92f8527487024c2a6a4fcc
Prof11 <prof36789@gmail.com>
2025-02-25 16:37:21 +0500

    Sprint2

--- app/src/main/java/com/example/professional/RegisterPage2.kt
+++ app/src/main/java/com/example/professional/RegisterPage2.kt
@@ -4,44 +4,76 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.PagerState
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.professional.ui.theme.ProfessionalTheme
+import com.example.professional.ui.theme.textColor
 
 class RegisterPage2 : ComponentActivity() {
+    @OptIn(ExperimentalFoundationApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            ProfessionalTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting2(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+            val pagerState = rememberPagerState { 4 }
+            val coroutineScope = rememberCoroutineScope()
+            Column (modifier = Modifier.fillMaxSize().padding(vertical = 40.dp),
+                verticalArrangement = Arrangement.SpaceBetween){
+                Column {
+                    Row (modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.Center){
+                        Text("Какова ваша цель?",
+                            color = Color.Black,
+                            fontSize = 20.sp)
+                    }
+                    Row (modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.Center){
+                        Text("Это поможет нам подобрать",
+                            color = textColor,
+                            fontSize = 12.sp)
+                    }
+                    Row (modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.Center){
+                        Text("для вас лучшую программу.",
+                            color = textColor,
+                            fontSize = 12.sp)
+                    }
+
+
                 }
             }
         }
     }
-}
+    @OptIn(ExperimentalFoundationApi::class)
+    @Composable
+    fun Pager1(pagerState: PagerState) {
+        HorizontalPager(pagerState) {page ->
+            when(page){
+                0->Screen1()
+                1->Screen2()
+                2->Screen3()
+                3->Screen4()
+            }
 
-@Composable
-fun Greeting2(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+        }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview2() {
-    ProfessionalTheme {
-        Greeting2("Android")
     }
+
 }
